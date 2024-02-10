81d1c24d4d4b33f8e84ae1155af9c83370dc0ba4
Baovola <baovolarav@gmail.com>
2024-02-10 17:30:14 +0100

    fixe email valid unique

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,22 @@
 import Form from "../models/Form.js";
 
 
-export const postMessage = async(req, res,next)=>{
-    const{name,email, message} = req.body;
+export const postMessage = async (req, res, next) => {
+    const { name, email, message } = req.body;
     try {
-         const newMessage = new Form({
-            name,
-            email,
-            message
-         });
-         await newMessage.save();
-         res.status(200).json("Message send successfully");
+      const newMessage = new Form({
+        name,
+        email,
+        message
+      });
+      await newMessage.save();
+      res.status(200).json("Message send successfully");
     } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "Un message avec cet email a déjà été envoyé." });
+      } else {
         next(error);
+      }
     }
-}
+  };
+  
